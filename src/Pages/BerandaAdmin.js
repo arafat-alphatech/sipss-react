d8161463aae8b4148339723515c52a5c72d09ec6
arafat <arafat@alphatech.id>
2018-11-30 18:11:20 +0700

    fix bug redirect

--- src/Pages/BerandaAdmin.js
+++ src/Pages/BerandaAdmin.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
 import MenuBawah from '../Components/MenuBawah'
 import { connect } from "unistore/react";
 import { actions } from "../store";
